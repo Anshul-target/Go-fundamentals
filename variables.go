dff0c6dd75a0c1f88a7268a15aa78b917b229bcb
Anshul <ayushyadav6a@gmail.com>
2025-12-08 22:25:08 +0530

    Completed the variables module

--- variables.go
+++ variables.go
@@ -1,4 +1,25 @@
+package main
+
+import "fmt"
+
 // Explicit type
-var x int =10; 
-var y int =20
-fmt.Println(a);
+
+func main()  {
+// var x int =10
+var y =0
+// Short variable declaration only included inside the functions
+// Walrus operator (nickname)
+s:="hello"
+fmt.Println(y,s)	
+var x *int=&y
+z:=12
+fmt.Println(x,z)
+redeclare()
+}
+
+func redeclare(){
+	
+	a:=2
+	a,b:=2,3
+	fmt.Println(a,b)
+}
